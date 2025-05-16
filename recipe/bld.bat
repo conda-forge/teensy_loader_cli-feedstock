b0e4ac2ac073737ec2f5b50233b888917e5706fc
Mark Harfouche <mark.harfouche@gmail.com>
2025-05-16 13:49:14 -0400

    Update bld.bat

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -1,7 +1,8 @@
 @echo ON
 setlocal enabledelayedexpansion
 
-make OS=WINDOWS CC=gcc
+:REM Taken from the makefile, for some reason it isn't finding the binaries in the path
+%CC% -O2 -Wall -s -DUSE_WIN32 -o teensy_loader_cli.exe teensy_loader_cli.c -lhid -lsetupapi -lwinmm
 if %ERRORLEVEL% neq 0 exit %ERRORLEVEL%
 
 mkdir %PREFIX%\bin
